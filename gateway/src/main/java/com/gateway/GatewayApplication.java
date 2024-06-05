65a6421514979c4b7ad23a813a09f47024cc1748
Vinicius Fillos <vinifillos@gmail.com>
2024-06-04 21:26:20 -0300

    feat: configurações iniciais implementadas

--- gateway/src/main/java/com/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/gateway/GatewayApplication.java
@@ -2,12 +2,25 @@ package com.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(r -> r.path("/funcionarios/**").uri("lb://msFuncionarios"))
+				.build();
 	}
 
 }
