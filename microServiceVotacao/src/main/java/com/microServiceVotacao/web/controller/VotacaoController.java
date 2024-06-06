79f80a1a1bed24939a2dec668f529e238e7b18dc
Keren Apuque <cardosokerenapuque@gmail.com>
2024-06-06 20:22:35 -0300

    refactor: alterado estrutora do código

--- microServiceVotacao/src/main/java/com/microServiceVotacao/web/controller/VotacaoController.java
+++ microServiceVotacao/src/main/java/com/microServiceVotacao/web/controller/VotacaoController.java
@@ -3,7 +3,6 @@ package com.microServiceVotacao.web.controller;
 import com.microServiceVotacao.entities.Votacao;
 import com.microServiceVotacao.services.VotacaoService;
 import com.microServiceVotacao.web.dto.ResultadoVotacaoDto;
-import com.microServiceVotacao.web.dto.VotoDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,22 +17,10 @@ public class VotacaoController {
 
     private final VotacaoService votacaoService;
 
-    @PatchMapping("/iniciar/{idProposta}")
-    public ResponseEntity<Votacao> iniciarVotacao (@PathVariable Long idProposta) {
-        Votacao votacao = votacaoService.iniciar(idProposta);
-        return new ResponseEntity<>(votacao, HttpStatus.CREATED);
-    }
-
     @PatchMapping("/encerrar/{idVotacao}")
     public ResponseEntity<ResultadoVotacaoDto> encerrarVotacao (@PathVariable Long idVotacao) {
         ResultadoVotacaoDto resultado = votacaoService.encerrar(idVotacao);
         return new ResponseEntity<>(resultado, HttpStatus.OK);
-    }
-
-    @PatchMapping("/{idVotacao}")
-    public ResponseEntity<VotoDto> votar (@PathVariable Long idVotacao ,@RequestBody VotoDto voto) {
-        VotoDto response = votacaoService.votar(idVotacao, voto);
-        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping
