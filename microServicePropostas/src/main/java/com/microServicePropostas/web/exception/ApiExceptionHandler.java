6eaa211c55918865a70b1954d5169292f5cb114c
Keren Apuque <cardosokerenapuque@gmail.com>
2024-06-05 21:32:25 -0300

    feat: adicinoado códigos nécessários para lançarem as exception

---
+++ microServicePropostas/src/main/java/com/microServicePropostas/web/exception/ApiExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.microServicePropostas.web.exception;
+
+
+import com.microServicePropostas.exception.EntityNotFoundException;
+import com.microServicePropostas.exception.EntityNullException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorMessage> entityNotFoundException(RuntimeException ex, HttpServletRequest request) {
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, ex.getMessage()));
+    }
+
+    @ExceptionHandler(EntityNullException.class)
+    public ResponseEntity<ErrorMessage> entityNullException(RuntimeException ex, HttpServletRequest request) {
+        log.error("Api Error - ", ex);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, ex.getMessage()));
+    }
+
+
+}
