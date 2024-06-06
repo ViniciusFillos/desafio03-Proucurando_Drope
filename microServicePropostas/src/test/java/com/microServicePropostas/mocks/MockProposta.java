080af71a37ab818a4be87bc0c48ebb381a21a725
Keren Apuque <cardosokerenapuque@gmail.com>
2024-06-06 09:23:43 -0300

    feat: adicionado testes unitátios da camada service

--- microServicePropostas/src/test/java/com/microServicePropostas/mocks/MockProposta.java
+++ microServicePropostas/src/test/java/com/microServicePropostas/mocks/MockProposta.java
@@ -1,2 +1,48 @@
-package com.microServicePropostas.mocks;public class MockFuncionario {
+package com.microServicePropostas.mocks;
+
+import com.microServicePropostas.entities.Proposta;
+import com.microServicePropostas.web.dto.PropostaDto;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockProposta {
+
+    public Proposta mockEntity(){
+        return mockEntity(0);
+    }
+
+    public PropostaDto mockDto(){
+        return mockDto(0);
+    }
+
+    public List<Proposta> mockEntityList() {
+        List<Proposta> propostas = new ArrayList<>();
+        for (int i = 1; i < 11; i++) {
+            propostas.add(mockEntity(i));
+        }
+        return propostas;
+    }
+
+    public List<PropostaDto> mockDtoList() {
+        List<PropostaDto> propostasDtos = new ArrayList<>();
+        for (int i = 1; i < 11; i++) {
+            propostasDtos.add(mockDto(i));
+        }
+        return propostasDtos;
+    }
+
+    public PropostaDto mockDto(Integer num) {
+        PropostaDto dto = new PropostaDto();
+        dto.setTitulo("Titulo do proposta " + num);
+        dto.setDescricao("Descricao do proposta " + num);
+        return dto;
+    }
+
+    public Proposta mockEntity(Integer num) {
+        Proposta proposta = new Proposta();
+        proposta.setId(num.longValue());
+        proposta.setTitulo("Titulo do proposta " + num);
+        proposta.setDescricao("Descricao do proposta " + num);
+        return proposta;
+    }
 }
