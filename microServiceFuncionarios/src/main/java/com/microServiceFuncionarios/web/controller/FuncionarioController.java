2c02d6e5a88f0d570ade7016dc59588d2515e4e3
nicoleinae <nicoleinae@gmail.com>
2024-06-09 22:49:17 -0300

    docs: implementação da documentação com swagger

--- microServiceFuncionarios/src/main/java/com/microServiceFuncionarios/web/controller/FuncionarioController.java
+++ microServiceFuncionarios/src/main/java/com/microServiceFuncionarios/web/controller/FuncionarioController.java
@@ -1,6 +1,13 @@
 package com.microServiceFuncionarios.web.controller;
 
 import com.microServiceFuncionarios.entities.Funcionario;
+import com.microServiceFuncionarios.web.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,46 +19,90 @@ import com.microServiceFuncionarios.web.dto.FuncionarioDto;
 
 import java.util.List;
 
+@Tag(name = "Micro Serviço Funcionários", description = "Contém todas as operações relativas aos recursos para criar, alterar, visualizar e deletar funcionários")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/funcionarios")
-
 public class FuncionarioController {
 
     private final FuncionarioService funcionarioService;
 
     @Transactional
     @PostMapping
+    @Operation(summary = "Criar um Funcionário", description = "Criar um Funcionário passando um JSON", tags = {"Funcionário"},
+            responses = {
+                    @ApiResponse(description = "Criado com Sucesso", responseCode =  "200", content = @Content(schema = @Schema(implementation = Funcionario.class))),
+                    @ApiResponse(description = "CPF já cadastrado", responseCode =  "409", content = @Content(schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(description = "Erro Interno", responseCode =  "500", content = @Content)
+            })
     public ResponseEntity<Funcionario> salvar (@RequestBody @Valid FuncionarioDto funcionario){
         Funcionario funcionarioCriado = funcionarioService.salvar(funcionario);
         return new ResponseEntity<>(funcionarioCriado, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar um Funcionário", description = "Buscar um Funcionário pelo id", tags = {"Funcionário"},
+            responses = {
+                    @ApiResponse(description = "Sucesso", responseCode =  "200", content = @Content(schema = @Schema(implementation = Funcionario.class))),
+                    @ApiResponse(description = "Requisição Inválida", responseCode =  "400", content = @Content),
+                    @ApiResponse(description = "Não Encontrado", responseCode =  "404", content = @Content),
+                    @ApiResponse(description = "Erro Interno", responseCode =  "500", content = @Content)
+            })
     public ResponseEntity<Funcionario> buscarPorId(@PathVariable Long id) {
         Funcionario funcionario = funcionarioService.buscarPorId(id);
         return new ResponseEntity<>(funcionario, HttpStatus.OK);
     }
 
     @GetMapping
+    @Operation(summary = "Buscar todos os Funcionários", description = "Buscar todos os Funcionários", tags = {"Funcionário"},
+            responses = {
+                    @ApiResponse(description = "Sucesso", responseCode =  "200",
+                            content = {
+                            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Funcionario.class)))
+                            }),
+                    @ApiResponse(description = "Requisição Inválida", responseCode =  "400", content = @Content),
+                    @ApiResponse(description = "Não Encontrado", responseCode =  "404", content = @Content),
+                    @ApiResponse(description = "Erro Interno", responseCode =  "500", content = @Content)
+            })
     public ResponseEntity<List<Funcionario>> buscarTodos() {
         List<Funcionario> todosFuncionarios = funcionarioService.buscarTodos();
         return new ResponseEntity<>(todosFuncionarios, HttpStatus.OK);
     }
 
-    @PatchMapping("inativar/{id}")
+    @PatchMapping("inativarfuncionario/{id}")
+    @Operation(summary = "Inativar um Funcionário", description = "Inativar um Funcionário pelo id", tags = {"Funcionário"},
+            responses = {
+                    @ApiResponse(description = "Atualizado", responseCode =  "200", content = @Content(schema = @Schema(implementation = Funcionario.class))),
+                    @ApiResponse(description = "Requisição Inválida", responseCode =  "400", content = @Content),
+                    @ApiResponse(description = "Não Encontrado", responseCode =  "404", content = @Content),
+                    @ApiResponse(description = "Erro Interno", responseCode =  "500", content = @Content)
+            })
     public ResponseEntity<Funcionario> inativarFuncionario(@PathVariable Long id) {
         Funcionario funcionarioinativo = funcionarioService.inativarFuncionario(id);
         return ResponseEntity.ok().body(funcionarioinativo);
     }
 
     @PutMapping("alterar/{id}")
+    @Operation(summary = "Alterar um Funcionário", description = "Alterar um Funcionário passando um JSON", tags = {"Funcionário"},
+            responses = {
+                    @ApiResponse(description = "Atualizado", responseCode =  "200", content = @Content(schema = @Schema(implementation = Funcionario.class))),
+                    @ApiResponse(description = "Requisição Inválida", responseCode =  "400", content = @Content),
+                    @ApiResponse(description = "Não Encontrado", responseCode =  "404", content = @Content),
+                    @ApiResponse(description = "Erro Interno", responseCode =  "500", content = @Content)
+            })
     public Funcionario alterarfuncionario (@PathVariable Long id, @RequestBody Funcionario funcionario) {
         Funcionario funcionarioalterado = funcionarioService.alterarFuncionario(id, funcionario);
         return new ResponseEntity<>(funcionarioalterado, HttpStatus.OK).getBody();
     }
 
     @DeleteMapping("deletar/{id}")
+    @Operation(summary = "Deletar um Funcionário", description = "Deletar um Funcionário por id", tags = {"Funcionário"},
+            responses = {
+                    @ApiResponse(description = "Sem Conteúdo", responseCode =  "204", content = @Content),
+                    @ApiResponse(description = "Requisição Inválida", responseCode =  "400", content = @Content),
+                    @ApiResponse(description = "Não Encontrado", responseCode =  "404", content = @Content),
+                    @ApiResponse(description = "Erro Interno", responseCode =  "500", content = @Content)
+            })
     public ResponseEntity<Void> deletarPorId(@PathVariable Long id) {
         funcionarioService.deletarfuncionario(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
